89286fcdcdca63e3e5eb2ae2804421101976c129
shivas1516 <sivasubramaniaml.ai2021@dce.edu.in>
2023-08-26 23:38:54 +0530

    try to use the util package

---
+++ inputjava.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class inputjava{
+	public static void main(String args[]){
+	 Scanner sc = new Scanner(System.in);
+	 
+	 System.out.println("Enter your name:");
+	 
+	 String name = sc.nextLine();
+	 
+	 System.out.println("Hello!"+name+", Good Morning");
+	 
+	 sc.close();
+	}
+}
